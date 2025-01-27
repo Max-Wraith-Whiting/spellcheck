74c5a97ba06052ae872bccf97937282576c4f445
Nexus <Max.Wraith-Whiting@protonmail.com>
2025-01-27 20:38:33 +0000

    Basic text input and trimming

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <ranges>
+#include <locale>
+
+
+std::string_view stripTailPunct(std::string_view s)
+{
+    for (size_t i = 0; i < s.length(); i++) {
+        if (std::ispunct(s.at(i))) {
+            s.remove_suffix(s.length() - i);
+            break;
+        }
+    }
+    // std::cout << "Trimmed: " << s << '\n';
+    
+    return s;
+}
+
+
+void repl()
+{
+    std::string input {};
+    std::cout << "~ ";
+    while (std::cin >> input) {
+        std::cout << "spellcheck: " << input << "\n~ ";
+        stripTailPunct(input);
+    }
+}
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    repl();
 }
