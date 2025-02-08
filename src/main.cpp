4a9f30b02415a205b47c90a4983f9dc08d31e5d2
Nexus <Max.Wraith-Whiting@protonmail.com>
2025-02-08 00:28:47 +0000

    [Major]: Gutted main fn and removed class dupe - Spellcheck class as it has been extracted + Cleaned up printVector output as it was ugly. + Added basic command line args and flags to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,207 +3,146 @@
 #include <vector>
 #include <queue>
 #include <fstream>
+#include <iomanip>
+#include <exception>
 
 #include "input_parser.hpp"
+#include "bk_tree.hpp"
 
-namespace SpellCheck {
-
-    /// @brief Calculates the Levenshtein distance between two strings.
-    /// Does not care about whether a or b is longer.
-    /// @param a First string to compare.
-    /// @param b Second string to compare.
-    /// @return Integer value for the Levenshtein distance.
-    int levDistance(const std::string_view a, const std::string_view b)
-    {
-        const size_t m {a.length() + 1};
-        const size_t n {b.length() + 1};
-
-        std::vector<int> v0(n);
-        std::vector<int> v1(n, 0);
-
-        for (size_t i {0}; i < n; i++)
-            v0.at(i) = static_cast<int>(i);
-
-        for (size_t i {0}; i < m - 1; i++) {
-            v1.at(0) = static_cast<int>(i) + 1;
-
-            for (size_t j {0}; j < n - 1; j++) {
-
-                int deleteCost {v0.at(j + 1) + 1};
-                int insertCost {v1.at(j) + 1};
-                int subCost {a.at(i) == b.at(j) ? v0.at(j) : v0.at(j) + 1};
-
-                v1.at(j + 1) = std::min({deleteCost, insertCost, subCost});
-            }
-
-            v0.swap(v1);
-        }
-
-        return v0.at(n - 1);
-    }
-
-    class BKTree {
-    private:
-        int lDistance {0};
-        std::string m_value {};
-        std::vector<BKTree*> m_children {};
-
-        void queryRecursive(std::vector<std::string_view>& output, std::string_view query, const int accuracy)
-        {
-            const int distance {levDistance(m_value, query)};
-            const int upperBound {distance + accuracy};
-            const int lowerBound {distance - accuracy};
-
-            // If an exact match is found, return only that.
-            if (distance == 0) {
-                output.clear();
-                output.push_back(m_value);
-                return;
-            }
-
-            // If a word within accuracy distance is found, append it to the output.
-            if (distance <= accuracy)
-                output.push_back(m_value);
-
-            // Query all the children of the current node that are within accuracy distance.
-            for (BKTree* child : m_children) {
-                if (child->lDistance <= upperBound && child->lDistance >= lowerBound)
-                    child->queryRecursive(output, query, accuracy);
-            }
-        }
-
-    public:
-
-        BKTree() = delete;
-        BKTree(std::string value) :
-            m_value {value}
-        {
-        }
-
-        // Use a stream to initialise and populate the BK-Tree.
-        BKTree(std::istream& stream)
-        {
-            stream >> m_value;
-
-            std::string s {};
-            while (stream >> s)
-                this->addChild(s);
-        }
-
-        ~BKTree() {
-            for (BKTree* child : m_children)
-                delete child;
-        }
-
-        /// @brief Adds a child node to the BKTree.
-        /// @param value A string value copy. (No need to worry about references or lifetimes etc.)
-        void addChild(std::string value) 
-        {
-            int distance {levDistance(m_value, value)};
-
-            for (BKTree* child : m_children) {
-                if (distance == child->lDistance) {
-                    child->addChild(value);
-                    return;
-                }
-            }
-
-            BKTree* newChild = new BKTree(value);
-            newChild->lDistance = levDistance(m_value, newChild->m_value);
-            m_children.emplace_back(newChild);
-        }
-
-        std::vector<std::string_view> query(std::string_view query, const int accuracy)
-        {
-            std::vector<std::string_view> output {};
-            output.reserve(16);
-
-            queryRecursive(output, query, accuracy);
-
-            return output;
-        }
-
-        void print()
-        {
-            std::queue<BKTree*> q {};
-            q.push(this);
-            while (!q.empty()) {
-                BKTree* current {q.front()};
-                q.pop();
-                std::cout << current->m_value << '\n';
-                for (BKTree* child : current->m_children)
-                    q.push(child);
-            }
-        }
-    
-        /// @brief Adds the contents of a stream by standard string extraction from a given stream. (I.e., stream >> s)
-        /// @param stream A standard `std::istream` reference. 
-        void populateFromStream(std::istream& stream)
-        {
-            std::string s {};
-            while (stream >> s) {
-                this->addChild(s);
-            }
-        }
-
-    };
-}
+namespace Config {
+    std::string g_dictLocation {"./dictionary/dict.txt"};
+};
 
 // DEBUG
 void printVector(std::vector<std::string_view> v)
 {
     std::cout << "< ";
+    if (v.size() == 0) {
+        std::cout << " >\n";
+        return;
+    } else if (v.size() == 1) {
+        std::cout << v.at(0) << " >\n";
+        return;
+    } 
+        
     for (std::string_view s : v) {
         std::cout << s << ", ";
     }
-    std::cout << ">\n";
+
+    // int size {static_cast<int>(v.size())};
+
+    for (size_t i {0}; i < v.size(); ++i) {
+        std::cout << v.at(i);
+        if (i != (v.size() - 1))
+            std::cout << ", ";
+    }
+    std::cout << " >\n";
 }
 
 // Possibly remove this as it is unecessary.
 std::string_view stripTailPunct(std::string_view s)
 {
-    for (size_t i = 0; i < s.length(); i++) {
-        if (std::ispunct(s.at(i))) {
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        if (std::ispunct(s.at(i)))
+        {
             s.remove_suffix(s.length() - i);
             break;
         }
     }
-    
+
     return s;
 }
 
-void repl()
+void repl(int accuracy=1)
 {
     std::ifstream file("./docs/british-english.txt");
 
     if (!file.is_open())
         std::cout << "File failed to open!";
 
-    SpellCheck::BKTree spellchecker(file);
-    std::string input {};
+    // SpellCheck::BKTree spellchecker(file);
+    BKTree spellchecker(file);
+    std::string input{};
     std::cout << "~ ";
     while (std::cin >> input) {
         std::cout << "spellcheck: " << input << "\n~ ";
-        std::vector<std::string_view> results {spellchecker.query(input, 1)};
+        std::vector<std::string_view> results{spellchecker.query(input, accuracy)};
         printVector(results);
     }
 }
 
-int main(int argc, char* argv[])
+void printHelp()
+{
+    std::cout << "Usage: spellcheck [OPTION]\n\n"
+            << std::setw(20) 
+            << "-h,  --help       print this help.\n"
+            << "-f,  --file       specify a file to spellcheck.\n"
+            << "-a,  --accuracy   specify an accuracy for the spellchecker.\n"
+            << "-r,  --repl       launch a spelling REPL.\n";
+}
+
+void handleFile(const InputParser& input, const std::string& option, const int accuracy=1)
+{
+    const std::string fileName{input.getOption(option)};
+    if (fileName.empty())
+        throw std::runtime_error("No file name provided!\n");
+
+    std::ifstream stream(fileName);
+    if (!stream.is_open())
+        throw std::runtime_error("Cannot find file: " + fileName + "!\n");
+
+    
+    std::ifstream dict(Config::g_dictLocation);
+    if (!dict.is_open())
+        throw std::runtime_error("Cannot open dictionary! Validate spellchecker/dictionary/dict.txt\n");
+
+    BKTree spellchecker(dict);
+    std::string s {};
+    while (stream >> s) {
+       std::cout << s << ": ";
+       printVector(spellchecker.query(s, accuracy));
+    }
+    
+}
+
+int main(int argc, char *argv[])
 {
     InputParser input(argc, argv);
 
+    if (input.isOption("-h") || input.isOption("--help")) {
+        printHelp();
+        return 0;
+    }
+
+    int accuracy {1};
+    
     if (input.isOption("-f")) {
-        const std::string fileName {input.getOption("-f")};
-        std::cout << "The file name is: " << fileName << "!\n";
+        const std::string fileName{input.getOption("-f")};
+        std::cout << "File: " << fileName << '\n';
+        try {
+            handleFile(input, "-f", accuracy);
+        } catch(const std::exception& e) {
+            std::cout << "Error: " << e.what();
+        }
         return 0;
-    }
 
-    if (input.isOption("-h")) {
-        std::cout << "HELP WAS INVOKED\n";
+    } else if (input.isOption("--file")) {
+        const std::string fileName{input.getOption("--file")};
+        std::cout << "File:" << fileName << '\n';
+        try {
+            handleFile(input, "--file", accuracy);
+        } catch (const std::exception& e) {
+            std::cout << "Error: " << e.what();
+        }
         return 0;
-    }
 
-    // repl();
-    std::cout << "Standard run!\n";
+    } else if (input.isOption("-r") || input.isOption("--repl")) {
+        std::cout << "REPL\n";
+        repl(accuracy);
+        return 0;
+
+    } else {
+        std::cout << "SPELLCHECK ONE WORD\n";
+    }
 }
